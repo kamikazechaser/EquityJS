95b83eaf2ac086f33cd43d0c5299a232703c8959
Mohammed Sohail <sohailsameja@gmail.com>
2016-12-09 18:32:10 +0530

    Token Test

--- test.js
+++ test.js
@@ -14,7 +14,6 @@
      *
      */
 const should = require('should');
-const fs = require('fs');
 const Equity = require('.');
 
 const timeout = 10 * 1000;
@@ -77,7 +76,6 @@ describe('Methods', function() {
         equity.getToken({ username: 'equityjs', password: 'equityjs', grant_type: 'password' }, function(e, cb) {
             should(cb.token_type).eql(tokenType);
             should(cb.expires_in).eql(expiryTime);
-            fs.writeFile('./.env', 'TOKEN=' + cb.access_token);
             return done();
         });
     });
